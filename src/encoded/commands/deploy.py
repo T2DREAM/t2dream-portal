e319b48981182fbf2c5512f67c27203860d73067
KJG <kgaulton@gmail.com>
2017-05-25 16:31:46 -0700

    config edits

--- src/encoded/commands/deploy.py
+++ src/encoded/commands/deploy.py
@@ -190,7 +190,7 @@ def create_ec2_instances(client, image_id, count, instance_type, security_groups
         MinCount=count,
         MaxCount=count,
         InstanceType=instance_type,
-       # SecurityGroups=security_groups,
+        SecurityGroups=security_groups,
         UserData=user_data,
         BlockDeviceMappings=bdm,
         InstanceInitiatedShutdownBehavior='terminate',
@@ -276,7 +276,7 @@ def run(wale_s3_prefix, image_id, instance_type, elasticsearch, spot_instance, s
         if cluster_name:
             data_insert['CLUSTER_NAME'] = cluster_name
         user_data = user_data % data_insert
-        security_groups = 'ssh,http,https'
+        security_groups = ['ssh','http','https']
         iam_role = 'encoded-instance'
         count = 1
     else:
