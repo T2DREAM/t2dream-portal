4cd69f7e66bb0c6505a9b99a86cd02c2641b1fb6
Parul-Kudtarkar <parulbioinf@gmail.com>
2018-01-08 12:45:49 -0800

    t Please enter the commit message for your changes. Lines starting

--- src/encoded/peak_indexer.py
+++ src/encoded/peak_indexer.py
@@ -33,6 +33,9 @@ _INDEXED_DATA = {
     'DNase-seq': {
         'file_type': ['bed narrowPeak']
     },
+    'chromatin state': {
+        'file_type': ['bed bed3+']
+        },
     'ATAC-seq': {
         'output_type': ['peaks']
     }
@@ -94,14 +97,16 @@ def index_settings():
     }
 
 
-def get_assay_term_name(accession, request):
+def get_annotation_type(accession, request):
     '''
-    Input file accession and returns assay_term_name of the experiment the file
+    Input file accession and returns annotation_type of the annotation the file
     belongs to
     '''
     context = request.embed(accession)
-    if 'assay_term_name' in context:
-        return context['assay_term_name']
+    log.debug('index accession: {}'.format(accession))
+    log.warn('index accession: {}'.format(accession))
+    if 'annotation_type' in context:
+        return context['annotation_type']
     return None
 
 
@@ -130,6 +135,7 @@ def index_peaks(uuid, request):
 
 
     if 'File' not in context['@type'] or 'dataset' not in context:
+        log.warn("File not in context type or dataset not in context: {}".format(pprint.pformat(context)))
         return
 
     if 'status' not in context or context['status'] != 'released':
@@ -138,14 +144,19 @@ def index_peaks(uuid, request):
     # Index human data for now
     if assembly not in _ASSEMBLIES:
         return
+<<<<<<< HEAD
 
     assay_term_name = get_assay_term_name(context['dataset'], request)
     if assay_term_name is None or isinstance(assay_term_name, collections.Hashable) is False:
+=======
+    annotation_type = get_annotation_type(context['dataset'], request)
+    if annotation_type is None or isinstance(annotation_type, collections.Hashable) is False:
+>>>>>>> parent of 0eaa93c... Region Search reverted
         return
 
     flag = False
 
-    for k, v in _INDEXED_DATA.get(assay_term_name, {}).items():
+    for k, v in _INDEXED_DATA.get(annotation_type, {}).items():
         if k in context and context[k] in v:
             if 'file_format' in context and context['file_format'] == 'bed':
                 flag = True
