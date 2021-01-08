6872abb1ddf9ab1260999296719a4a1edb531b08
Parul-Kudtarkar <ubuntu@ip-172-31-61-158.us-west-2.compute.internal>
2021-01-08 08:22:26 +0000

    remove virtual env

--- .t2dream-venv/lib/python2.7/site-packages/mr/developer/compat.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-if sys.version_info < (3, 0):
-    b = lambda x: x
-    s = lambda x: x
-else:
-    b = lambda x: bytes(x, "utf-8")
-    s = lambda x: str(x, "utf-8")
--- .t2dream-venv/lib/python2.7/site-packages/mr/developer/tests/test_cvs.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import doctest
-import mr.developer.cvs
-
-
-def test_suite():
-    return unittest.TestSuite([doctest.DocTestSuite(mr.developer.cvs)])
