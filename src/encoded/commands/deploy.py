f24d1420f228e9c3d88b2e20ab1e641be412361b
Parul-Kudtarkar <parulbioinf@gmail.com>
2021-02-08 06:31:17 +0000

    change security group

--- src/encoded/commands/deploy.py
+++ src/encoded/commands/deploy.py
@@ -328,7 +328,7 @@ def run(
             'CLUSTER_NAME': cluster_name,
         }
         user_data = get_user_data(commit, config_file, data_insert, profile_name)
-        security_groups = ['elasticsearch-https']
+        security_groups = ['ES']
         iam_role = 'elasticsearch-instance'
         count = int(cluster_size)
 
