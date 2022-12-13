27db39bd0154cecf4129eaee8c9212015a2ccf25
Parul-Kudtarkar <parulbioinf@gmail.com>
2022-12-13 20:42:30 +0000

    update ami to bionic

--- src/encoded/commands/deploy.py
+++ src/encoded/commands/deploy.py
@@ -404,8 +404,8 @@ def main():
         '--test', action='store_const', default='demo', const='test', dest='role',
         help="Deploy to production AWS")
     parser.add_argument(
-        '--image-id', default='ami-2133bc59',
-        help="https://us-west-2.console.aws.amazon.com/ec2/home?region=us-west-2#LaunchInstanceWizard:ami=ami-2133bc59")
+        '--image-id', default='ami-0d1cd67c26f5fca19',
+        help="https://us-west-2.console.aws.amazon.com/ec2/home?region=us-west-2#LaunchInstanceWizard:ami=ami-0d1cd67c26f5fca19")
     parser.add_argument(
         '--instance-type', default='c5.9xlarge',
         help="(defualts to c5.9xlarge for indexing) Switch to a smaller instance afterwards"
